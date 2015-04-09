17a6ebfb4709fa183bbd333410c46bda1c610d8e
yoshelynfielding <yoshelynf@gmail.com>
2015-04-09 13:33:14 +0100

    Adding second line to the console
    some description

--- GitHubConsole/GitHubConsole/Program.cs
+++ GitHubConsole/GitHubConsole/Program.cs
@@ -11,6 +11,7 @@ namespace GitHubConsole
         static void Main(string[] args)
         {
             Console.Write("Hello Git");
+            Console.Write("Hello Git branching");
             Console.Read();
         }
     }
